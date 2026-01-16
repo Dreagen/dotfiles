1346779e9cad53f269ec162c449e86c04aaf953d
Nick Allen <nick.allen@dataglide.co>
2026-01-16 17:54:00 +0000

    Move to blink-cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -634,8 +634,10 @@ require('lazy').setup({
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -749,191 +751,191 @@ require('lazy').setup({
     },
   },
 
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-      },
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-    },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
-
-      -- Set up vim-dadbod
-      cmp.setup.filetype({ 'sql' }, {
-        sources = {
-          { name = 'vim-dadbod-completion' },
-          -- Add if you want completion for other items in the buffer
-          -- { name = 'buffer' },
-        },
-      })
-
-      local sqlcmd_env = {
-        fixed_width = vim.env.SQLCMDMAXFIXEDTYPEWIDTH or nil,
-        var_width = vim.env.SQLCMDMAXVARTYPEWIDTH or nil,
-      }
-      --
-      -- Function to enable wide column mode
-      local function enable_wide_columns()
-        vim.env.SQLCMDMAXFIXEDTYPEWIDTH = '12000'
-        vim.env.SQLCMDMAXVARTYPEWIDTH = '12000'
-        print 'Wide column mode enabled for sqlcmd'
-      end
-
-      -- Function to reset to default column mode
-      local function disable_wide_columns()
-        vim.env.SQLCMDMAXFIXEDTYPEWIDTH = sqlcmd_env.fixed_width
-        vim.env.SQLCMDMAXVARTYPEWIDTH = sqlcmd_env.var_width
-        print 'Wide column mode disabled for sqlcmd'
-      end
-
-      -- Toggle function for convenience
-      local wide_columns_enabled = false
-      local function toggle_wide_columns()
-        if wide_columns_enabled then
-          disable_wide_columns()
-        else
-          enable_wide_columns()
-        end
-        wide_columns_enabled = not wide_columns_enabled
-      end
-      --
-      -- Expose functions for easy mapping or use
-      vim.api.nvim_create_user_command('EnableWideColumns', enable_wide_columns, {})
-      vim.api.nvim_create_user_command('DisableWideColumns', disable_wide_columns, {})
-      vim.api.nvim_create_user_command('ToggleWideColumns', toggle_wide_columns, {})
-
-      vim.api.nvim_create_user_command('ExtractMappedJson', function()
-        -- Get the current buffer content
-        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-        local json_line = table.concat(lines, '\n')
-
-        -- Find the JSON within the MappedJSON field
-        local json_content = json_line:match '"MappedJSON":"({.-})"'
-        if json_content then
-          -- Replace escaped quotes with normal quotes
-          json_content = json_content:gsub('\\"', '"')
-
-          -- Remove excessive whitespace
-          json_content = json_content:gsub('%s+', ' ')
-
-          -- Fix url slashes
-          json_content = json_content:gsub('\\\\/', '/')
-
-          -- Clear the buffer and set the new JSON content
-          vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(json_content, '\n'))
-
-          vim.bo.filetype = 'json'
-          vim.defer_fn(function()
-            vim.lsp.buf.format { async = true }
-          end, 200)
-          -- vim.cmd 'normal! <leader>f'
-          -- vim.cmd 'silent! %!jq .'
-        else
-          print 'No JSON object found in MappedJSON field'
-        end
-      end, {})
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noinsert' },
-
-        -- For an understanding of why these mappings were
-        -- chosen, you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          -- Scroll the documentation window [b]ack / [f]orward
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          -- Accept ([y]es) the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-          -- If you prefer more traditional completion keymaps,
-          -- you can uncomment the following lines
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-          -- ['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-          -- Manually trigger a completion from nvim-cmp.
-          --  Generally you don't need this, because nvim-cmp will display
-          --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
-
-          -- Think of <c-l> as moving to the right of your snippet expansion.
-          --  So if you have a snippet that's like:
-          --  function $name($args)
-          --    $body
-          --  end
-          --
-          -- <c-l> will move you to the right of each of the expansion locations.
-          -- <c-h> is similar, except moving you backwards.
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        },
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-        },
-      }
-    end,
-  },
+  -- { -- Autocompletion
+  --   'hrsh7th/nvim-cmp',
+  --   event = 'InsertEnter',
+  --   dependencies = {
+  --     -- Snippet Engine & its associated nvim-cmp source
+  --     {
+  --       'L3MON4D3/LuaSnip',
+  --       build = (function()
+  --         -- Build Step is needed for regex support in snippets.
+  --         -- This step is not supported in many windows environments.
+  --         -- Remove the below condition to re-enable on windows.
+  --         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+  --           return
+  --         end
+  --         return 'make install_jsregexp'
+  --       end)(),
+  --       dependencies = {
+  --         -- `friendly-snippets` contains a variety of premade snippets.
+  --         --    See the README about individual language/framework/plugin snippets:
+  --         --    https://github.com/rafamadriz/friendly-snippets
+  --         -- {
+  --         --   'rafamadriz/friendly-snippets',
+  --         --   config = function()
+  --         --     require('luasnip.loaders.from_vscode').lazy_load()
+  --         --   end,
+  --         -- },
+  --       },
+  --     },
+  --     'saadparwaiz1/cmp_luasnip',
+  --
+  --     -- Adds other completion capabilities.
+  --     --  nvim-cmp does not ship with all sources by default. They are split
+  --     --  into multiple repos for maintenance purposes.
+  --     'hrsh7th/cmp-nvim-lsp',
+  --     'hrsh7th/cmp-path',
+  --   },
+  --   config = function()
+  --     -- See `:help cmp`
+  --     local cmp = require 'cmp'
+  --     local luasnip = require 'luasnip'
+  --     luasnip.config.setup {}
+  --
+  --     -- Set up vim-dadbod
+  --     cmp.setup.filetype({ 'sql' }, {
+  --       sources = {
+  --         { name = 'vim-dadbod-completion' },
+  --         -- Add if you want completion for other items in the buffer
+  --         -- { name = 'buffer' },
+  --       },
+  --     })
+  --
+  --     local sqlcmd_env = {
+  --       fixed_width = vim.env.SQLCMDMAXFIXEDTYPEWIDTH or nil,
+  --       var_width = vim.env.SQLCMDMAXVARTYPEWIDTH or nil,
+  --     }
+  --     --
+  --     -- Function to enable wide column mode
+  --     local function enable_wide_columns()
+  --       vim.env.SQLCMDMAXFIXEDTYPEWIDTH = '12000'
+  --       vim.env.SQLCMDMAXVARTYPEWIDTH = '12000'
+  --       print 'Wide column mode enabled for sqlcmd'
+  --     end
+  --
+  --     -- Function to reset to default column mode
+  --     local function disable_wide_columns()
+  --       vim.env.SQLCMDMAXFIXEDTYPEWIDTH = sqlcmd_env.fixed_width
+  --       vim.env.SQLCMDMAXVARTYPEWIDTH = sqlcmd_env.var_width
+  --       print 'Wide column mode disabled for sqlcmd'
+  --     end
+  --
+  --     -- Toggle function for convenience
+  --     local wide_columns_enabled = false
+  --     local function toggle_wide_columns()
+  --       if wide_columns_enabled then
+  --         disable_wide_columns()
+  --       else
+  --         enable_wide_columns()
+  --       end
+  --       wide_columns_enabled = not wide_columns_enabled
+  --     end
+  --     --
+  --     -- Expose functions for easy mapping or use
+  --     vim.api.nvim_create_user_command('EnableWideColumns', enable_wide_columns, {})
+  --     vim.api.nvim_create_user_command('DisableWideColumns', disable_wide_columns, {})
+  --     vim.api.nvim_create_user_command('ToggleWideColumns', toggle_wide_columns, {})
+  --
+  --     vim.api.nvim_create_user_command('ExtractMappedJson', function()
+  --       -- Get the current buffer content
+  --       local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  --       local json_line = table.concat(lines, '\n')
+  --
+  --       -- Find the JSON within the MappedJSON field
+  --       local json_content = json_line:match '"MappedJSON":"({.-})"'
+  --       if json_content then
+  --         -- Replace escaped quotes with normal quotes
+  --         json_content = json_content:gsub('\\"', '"')
+  --
+  --         -- Remove excessive whitespace
+  --         json_content = json_content:gsub('%s+', ' ')
+  --
+  --         -- Fix url slashes
+  --         json_content = json_content:gsub('\\\\/', '/')
+  --
+  --         -- Clear the buffer and set the new JSON content
+  --         vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(json_content, '\n'))
+  --
+  --         vim.bo.filetype = 'json'
+  --         vim.defer_fn(function()
+  --           vim.lsp.buf.format { async = true }
+  --         end, 200)
+  --         -- vim.cmd 'normal! <leader>f'
+  --         -- vim.cmd 'silent! %!jq .'
+  --       else
+  --         print 'No JSON object found in MappedJSON field'
+  --       end
+  --     end, {})
+  --
+  --     cmp.setup {
+  --       snippet = {
+  --         expand = function(args)
+  --           luasnip.lsp_expand(args.body)
+  --         end,
+  --       },
+  --       completion = { completeopt = 'menu,menuone,noinsert' },
+  --
+  --       -- For an understanding of why these mappings were
+  --       -- chosen, you will need to read `:help ins-completion`
+  --       --
+  --       -- No, but seriously. Please read `:help ins-completion`, it is really good!
+  --       mapping = cmp.mapping.preset.insert {
+  --         -- Select the [n]ext item
+  --         ['<C-n>'] = cmp.mapping.select_next_item(),
+  --         -- Select the [p]revious item
+  --         ['<C-p>'] = cmp.mapping.select_prev_item(),
+  --
+  --         -- Scroll the documentation window [b]ack / [f]orward
+  --         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  --
+  --         -- Accept ([y]es) the completion.
+  --         --  This will auto-import if your LSP supports it.
+  --         --  This will expand snippets if the LSP sent a snippet.
+  --         -- ['<C-y>'] = cmp.mapping.confirm { select = true },
+  --
+  --         -- If you prefer more traditional completion keymaps,
+  --         -- you can uncomment the following lines
+  --         ['<CR>'] = cmp.mapping.confirm { select = true },
+  --         -- ['<Tab>'] = cmp.mapping.select_next_item(),
+  --         --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  --
+  --         -- Manually trigger a completion from nvim-cmp.
+  --         --  Generally you don't need this, because nvim-cmp will display
+  --         --  completions whenever it has completion options available.
+  --         ['<C-Space>'] = cmp.mapping.complete {},
+  --
+  --         -- Think of <c-l> as moving to the right of your snippet expansion.
+  --         --  So if you have a snippet that's like:
+  --         --  function $name($args)
+  --         --    $body
+  --         --  end
+  --         --
+  --         -- <c-l> will move you to the right of each of the expansion locations.
+  --         -- <c-h> is similar, except moving you backwards.
+  --         ['<C-l>'] = cmp.mapping(function()
+  --           if luasnip.expand_or_locally_jumpable() then
+  --             luasnip.expand_or_jump()
+  --           end
+  --         end, { 'i', 's' }),
+  --         ['<C-h>'] = cmp.mapping(function()
+  --           if luasnip.locally_jumpable(-1) then
+  --             luasnip.jump(-1)
+  --           end
+  --         end, { 'i', 's' }),
+  --
+  --         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+  --         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+  --       },
+  --       sources = {
+  --         { name = 'nvim_lsp' },
+  --         { name = 'luasnip' },
+  --         { name = 'path' },
+  --       },
+  --     }
+  --   end,
+  -- },
 
   -- Colorschemes
   { -- You can easily change to a different colorscheme.
