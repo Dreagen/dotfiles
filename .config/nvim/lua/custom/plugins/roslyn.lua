96ed21671b3a3c24d0a48cba3f7eef5ed3f8f9b9
Nick Allen <nick.allen@dataglide.co>
2026-02-17 14:47:10 +0000

    Remove old razor config

--- .config/nvim/lua/custom/plugins/roslyn.lua
+++ .config/nvim/lua/custom/plugins/roslyn.lua
@@ -1,31 +1,9 @@
 return {
   'seblyng/roslyn.nvim',
   ft = { 'cs', 'razor' },
-  dependencies = {
-    {
-      -- By loading as a dependencies, we ensure that we are available to set
-      -- the handlers for Roslyn.
-      'tris203/rzls.nvim',
-      config = true,
-    },
-  },
   config = function()
-    -- Mason configuration for rzls
-    local rzls_path = vim.fn.expand '$MASON/packages/rzls/libexec'
-    local cmd = {
-      'roslyn',
-      '--stdio',
-      '--logLevel=Information',
-      '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
-      '--razorSourceGenerator=' .. vim.fs.joinpath(rzls_path, 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
-      '--razorDesignTimePath=' .. vim.fs.joinpath(rzls_path, 'Targets', 'Microsoft.NET.Sdk.Razor.DesignTime.targets'),
-      '--extension',
-      vim.fs.joinpath(rzls_path, 'RazorExtension', 'Microsoft.VisualStudioCode.RazorExtension.dll'),
-    }
-
     vim.lsp.config('roslyn', {
       cmd = cmd,
-      handlers = require 'rzls.roslyn_handlers',
       settings = {
         ['csharp|background_analysis'] = {
           dotnet_analyzer_diagnostics_scope = 'openFiles',
@@ -56,14 +34,5 @@ return {
       },
     })
     vim.lsp.enable 'roslyn'
-  end,
-  init = function()
-    -- We add the Razor file types before the plugin loads.
-    vim.filetype.add {
-      extension = {
-        razor = 'razor',
-        cshtml = 'razor',
-      },
-    }
   end,
 }
