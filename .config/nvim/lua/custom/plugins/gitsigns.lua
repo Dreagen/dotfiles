c491f07c436f4a22318191ac621f7f7811fd0317
Nick Allen <nick.allen@dataglide.co>
2026-04-08 17:10:47 +0100

    Add keyboard shortcuts for git hunks

--- .config/nvim/lua/custom/plugins/gitsigns.lua
+++ .config/nvim/lua/custom/plugins/gitsigns.lua
@@ -6,5 +6,20 @@ return {
       ':lua require("gitsigns").preview_hunk()<CR>',
       desc = 'See hunk diff',
     },
+    {
+      '<leader>gr',
+      ':lua require("gitsigns").reset_hunk()<CR>',
+      desc = 'Reset hunk',
+    },
+    {
+      '<leader>gn',
+      ':lua require("gitsigns").next_hunk()<CR>',
+      desc = 'Next hunk',
+    },
+    {
+      '<leader>gp',
+      ':lua require("gitsigns").prev_hunk()<CR>',
+      desc = 'Previous hunk',
+    },
   },
 }
