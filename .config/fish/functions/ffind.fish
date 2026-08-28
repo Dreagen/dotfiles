6f65937393c010786647cd93182f0d1769c1c8b7
Nick Allen <nick.allen@dataglide.co>
2026-08-28 10:29:03 +0100

    Improve ffind function in fish for better tmux window handling

--- .config/fish/functions/ffind.fish
+++ .config/fish/functions/ffind.fish
@@ -7,7 +7,16 @@ function ffind
     if test -n "$TMUX"
         tmux rename-session (basename $dir)
         tmux rename-window code
-        tmux new-window -d -n terminal -c $dir
-        tmux new-window -d -n ai -c $dir
+
+        set -l session (tmux display-message -p '#S')
+        set -l cd_command "cd -- "(string escape -- $dir)
+
+        for window in terminal ai
+            if tmux list-windows -F '#W' | string match -q -- $window
+                tmux send-keys -t "$session:$window" C-c "$cd_command" Enter
+            else
+                tmux new-window -d -n $window -c $dir
+            end
+        end
     end
 end
