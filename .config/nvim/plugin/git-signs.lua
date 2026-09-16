0e871eff221926739ddf4bb5cb83616bd539cc9d
Nick Allen <nick.allen@dataglide.co>
2026-09-16 09:12:44 +0100

    Nvim: Add more git functionality through keyboard shortcuts

--- .config/nvim/plugin/git-signs.lua
+++ .config/nvim/plugin/git-signs.lua
@@ -1,1 +1,15 @@
 vim.pack.add({ 'https://github.com/lewis6991/gitsigns.nvim' })
+
+local gs = require('gitsigns')
+
+vim.keymap.set('n', '<leader>gn', function()
+    gs.nav_hunk('next')
+end, { desc = 'Next Git hunk' })
+
+vim.keymap.set('n', '<leader>gp', function()
+    gs.nav_hunk('prev')
+end, { desc = 'Previous Git hunk' })
+
+vim.keymap.set('v', '<leader>gd', function()
+    gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+end, { desc = 'Discard selected Git lines' })
